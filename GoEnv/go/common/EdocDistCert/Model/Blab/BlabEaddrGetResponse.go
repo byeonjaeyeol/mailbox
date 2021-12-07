309c56525783c30d1c76ae6ab5282c7f471a213a
KimHaebo <haebo@bsquarelab.com>
2021-12-07 11:01:07 +0900

    * 파일명 오타 수정 * 응답에서 resultCode가 1이 아니면 errCode, errMsg 없음 설몇 추가 * 배열 예제 수정 * 오타 수정

--- GoEnv/go/common/EdocDistCert/Model/Blab/BlabEaddrGetResponse.go
+++ GoEnv/go/common/EdocDistCert/Model/Blab/BlabEaddrGetResponse.go
@@ -4,16 +4,16 @@ package Blab
 type BlabEaddrGetResponse struct {
 	// resultCode 처리결과 (1:성공, 0:실패)
 	ResultCode int8 `json:"resultCode" enums:"0,1" example:"1" validate:"required"`
-	// errCode 오류코드
+	// errCode 오류코드, resultCode가 1이 아니면 errCode, errMsg 항목은 없음
 	ErrCode string `json:"errCode,omitempty" example:"ERR-01-304" maxLength:"10" validatee:"required"`
-	// errMsg 오류메시지
-	ErrMsg string `json:"errMsg,omitempty" example:"공인전자주소가 존재하지 않습니다." maxLength:"256""`
+	// errMsg 오류메시지, resultCode가 1이 아니면 errCode, errMsg 항목은 없음
+	ErrMsg string `json:"errMsg,omitempty" example:"공인전자주소가 존재하지 않습니다." maxLength:"256"`
 
 	// name 이용자 명
-	Name string `json:"name,omitempty" example:"한국인터넷진흥원" maxLength:"40""`
+	Name string `json:"name,omitempty" example:"한국인터넷진흥원" maxLength:"40"`
 
-	// eaddrs 이용자 명
-	Eaddrs []string `json:"eaddrs,omitempty" example:"[ \“kisa\”, \“한국인터넷진흥원_디지털문서팀\”]""`
+	// eaddrs 공인전자주소 배열
+	Eaddrs []string `json:"eaddrs,omitempty" example:"eaddr1,eaddr2"`
 }
 
 func (br *BlabEaddrGetResponse) SetResult(resultCode int8, errCode string, errMsg string) {
--- GoEnv/go/common/EdocDistCert/Model/Blab/BlabEdocDistRegistResponse.go
+++ GoEnv/go/common/EdocDistCert/Model/Blab/BlabEdocDistRegistResponse.go
@@ -5,10 +5,10 @@ type BlabEdocDistRegistResponse struct {
 	// resultCode 처리결과 (1:성공, 0:실패)
 	ResultCode int8 `json:"resultCode" enums:"0,1" example:"1" validate:"required"`
 
-	// errCode 오류코드
+	// errCode 오류코드, resultCode가 1이 아니면 errCode, errMsg 항목은 없음
 	ErrCode string `json:"errCode,omitempty" example:"ERR-01-304" maxLength:"10" validatee:"required"`
-	// errMsg 오류메시지
-	ErrMsg string `json:"errMsg,omitempty" example:"공인전자주소가 존재하지 않습니다." maxLength:"256""`
+	// errMsg 오류메시지, resultCode가 1이 아니면 errCode, errMsg 항목은 없음
+	ErrMsg string `json:"errMsg,omitempty" example:"공인전자주소가 존재하지 않습니다." maxLength:"256"`
 
 	// circulations 유통정보 배열
 	Circulations []BlabEdocDistRegistCircResponse `json:"circulations,omitempty" maxLength:"500" validate:"required"`
