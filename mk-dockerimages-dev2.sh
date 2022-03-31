f25d2a9bd53d445721d54dd01681357f60a774f0
ERIC KYEHYUK AHN <alwayswinners@gmail.com>
2022-03-31 11:00:23 +0900

    change configurations in directory

--- mk-dockerimages-dev2.sh
+++ mk-dockerimages-dev2.sh
@@ -2,8 +2,8 @@ docker build -f ./Basic/docker/Dockerfile -t bsquarelab/upost-basic:latest ./Bas
 docker build -f ./Node/docker/Dockerfile -t bsquarelab/upost-node:latest ./Node/docker
 docker build -f ./Service01/docker/Dockerfile -t bsquarelab/upost-service01:0.1 ./Service01/docker
 docker build -f ./Service02/docker/Dockerfile -t bsquarelab/upost-service02:0.1 ./Service02/docker
-docker build -f ./API-WAS1/docker-mailbox/Dockerfile -t bsquarelab/upost-node-mailbox:0.1 ./API-WAS1/docker-mailbox/
+docker build -f ./API-WAS1/docker-mailbox-service/Dockerfile -t bsquarelab/upost-node-mailbox:0.1 ./API-WAS1/docker-mailbox-service/
 docker build -f ./API-WAS1/docker-mailbox-front/Dockerfile -t bsquarelab/upost-node-mailbox-front:0.1 ./API-WAS1/docker-mailbox-front/
 docker build -f ./API-WAS1/docker-mailbox-front-user/Dockerfile -t bsquarelab/upost-node-mailbox-front-user:0.1 ./API-WAS1/docker-mailbox-front-user/
-docker build -f ./API-WAS1/docker-static/Dockerfile -t bsquarelab/upost-node-static:0.1 ./API-WAS1/docker-mailbox-front/
+docker build -f ./API-WAS1/docker-mailbox-static/Dockerfile -t bsquarelab/upost-node-static:0.1 ./API-WAS1/docker-mailbox-front/
 
--- mk-dockerimages-mac.sh
+++ mk-dockerimages-mac.sh
@@ -6,8 +6,8 @@ docker build -f ./External01/docker/Dockerfile -t bsquarelab/upost-external:0.1 
 docker build -f ./Service01/docker/Dockerfile -t bsquarelab/upost-service01:0.1 ./Service01/docker
 docker build -f ./Service02/docker/Dockerfile -t bsquarelab/upost-service02:0.1 ./Service02/docker
 docker build -f ./Service04/docker/Dockerfile -t bsquarelab/upost-service04:0.3 ./Service04/docker
-docker build -f ./API-WAS1/docker-mailbox/Dockerfile -t bsquarelab/upost-node-mailbox:0.1 ./API-WAS1/docker-mailbox/
+docker build -f ./API-WAS1/docker-mailbox-service/Dockerfile -t bsquarelab/upost-node-mailbox:0.1 ./API-WAS1/docker-mailbox-service/
 docker build -f ./API-WAS1/docker-mailbox-front/Dockerfile -t bsquarelab/upost-node-mailbox-front:0.1 ./API-WAS1/docker-mailbox-front/
 docker build -f ./API-WAS1/docker-mailbox-front-user/Dockerfile -t bsquarelab/upost-node-mailbox-front-user:0.1 ./API-WAS1/docker-mailbox-front-user/
-docker build -f ./API-WAS1/docker-static/Dockerfile -t bsquarelab/upost-node-static:0.1 ./API-WAS1/docker-mailbox-front/
+docker build -f ./API-WAS1/docker-mailbox-static/Dockerfile -t bsquarelab/upost-node-static:0.1 ./API-WAS1/docker-mailbox-front/
 
